f3e2cb5c237141e96859419388f54efbf04720e3
Lucas Cardial <luccascardial@gmail.com>
2026-05-16 09:29:29 -0300

    add comentários

--- main.c
+++ main.c
@@ -67,7 +67,6 @@ void start_tick_loop(int frequency)
     matriz[29][9] = 3;
 
     while (1) {
-        // Agora limpamos apenas a tela alternativa atual (sem mexer no seu histórico real)
         limpaTela();
         bool found = false;
 
@@ -78,39 +77,85 @@ void start_tick_loop(int frequency)
         }
         printf("total de frames: %d\n", counter);
         counter++;
-
+        /**
+         * O primeiro passo é encontrar onde na matriz está a snake.
+         * Por enquanto estou atuando apenas com a identificação da cabeça (SNAKE_HEAD).
+         *
+         * **/
         for (int i = 0; i < 40; i++) {
             for (int j = 0; j < 40; j++) {
+                /**
+                 * Esse IF sinaliza que encontramos a posição da SNAKE_HEAD na matriz.
+                 * **/
                 if (matriz[i][j] ==  SNAKE_HEAD) {
-                    if (ultima_tecla == 'w') {
+                    /**
+                     * E agora que encontramos, precisamos manipular a direção de movimento da SNAKE_HEAD.
+                     * Pra isso precisamos etender a ordem de renderização da matriz:
+                     * (i) refere-se à linha, enquanto (j) à coluna.
+                     *
+                     * Supondo que a SNAKE_HEAD esteja na posição i,j = [5,3] e a direção seja de subida,
+                     * então o único ponteiro a ser atualizado é o (i), mantendo o (j): [4,3].
+                     *
+                     * Dessa forma, quando falamos em "subir", estamos decrementando a posição (i).
+                     * Por lógica ao descer, estamos incrementando o (i).
+                     *
+                     * Direita deve incrementar o (j) e não alterar o (i): [4,3]; [4,4]; [4,5].
+                     * E da mesma forma, a esquerda decrementa o (j), sem alterar o (i): [4,5]; [4;4], [4;3].
+                     *
+                     * Essa regra se repete até que implementemos uma regra de colisão. Como não há colisão,
+                     * as posições SNAKE ficarão negativos e sumirão da tela, pois nosso MAPA é uma matriz 40X40.
+                     *
+                     **/
+                    if (ultima_tecla == 'w') { // w = CIMA
                         matriz[i][j] = 0;
                         matriz[i-1][j] = 1;
                     }
 
-                    if (ultima_tecla == 's') {
+                    if (ultima_tecla == 's') { // s = BAIXO
                         matriz[i][j] = 0;
                         matriz[i+1][j] = 1;
                     }
 
-                    if (ultima_tecla == 'a') {
+                    if (ultima_tecla == 'a') { // a = ESQUERDA
                         matriz[i][j] = 0;
                         matriz[i][j -1] = 1;
                     }
 
-                    if (ultima_tecla == 'd') {
+                    if (ultima_tecla == 'd') { // d = DIREITA
                         matriz[i][j] = 0;
                         matriz[i][j + 1] = 1;
                     }
+
+                    /**
+                     * Quando encontramos a SNAKE_HEAD precisamos parar de procurá-la na matriz.
+                     * Como estamos em um for(j) dentro de outro for(i), precisamos definir
+                     * a variável [found] = true, e só então sair do for(j) com break;
+                     *
+                     **/
                     found = true;
                     break;
                 }
-
+                /**
+                 * Ao sair do for(j) sinalizando o found = true, então ele será verificado no for(i) que também
+                 * forçará sua saída.
+                 ***/
                 if (found) break;
             }
         }
 
+        /**
+         * Aqui estamos desenhando o mapa que é uma matriz 40x40.
+         * As células vazias são as células que não contém partes da SNAKE, ou paredes e frutinhas que ainda
+         * vamos implementar.
+         * Toda célula vazia é printada com um espaço vazio, para que gere "volume" no tabuleiro. Do contrário os
+         * espaços vazios não existiriam e o movimento da snake ficaria errado.
+         * **/
         for (int i = 0; i < 40; i++) {
             for (int j = 0; j < 40; j++) {
+                /**
+                 * No passo anterior validamos a posição da HEAD e o seu deslocamento baseado na direção.
+                 * Aqui precisamos apenas renderizar a SNAKE_HEAD na matriz do mapa:
+                 ***/
                 if (matriz[i][j] ==  SNAKE_HEAD) {
                     printf("\033[42m H \033[0m");
                 } else {
